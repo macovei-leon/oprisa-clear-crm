2bc9194719e1d4ac9f3020a3a3a55324da822b06
Leon <macoveileon01@gmail.com>
2026-06-30 16:25:12 +0300

    chore: change port to 3051 for dev environment and resolve stash conflict

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hdzrwlejqhygqyatiqgn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkenJ3bGVqcWh5Z3F5YXRpcWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzU2MjAsImV4cCI6MjA5ODMxMTYyMH0.gHGptYkGtr_qVeNhQTLDG36KrmT05OibWbeJQ0LYKmw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
